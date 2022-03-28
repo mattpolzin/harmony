1ce06becc66daa9c2975929fb3ff1fcfd5afe607
Mathew Polzin <matt.polzin@gmail.com>
2022-03-27 20:55:20 -0700

    update version check script.

--- version-check.sh
+++ version-check.sh
@@ -1,12 +1,13 @@
 #/bin/sh
 
-idrversion="$(cat harmony.ipkg | sed -n 's/version = \(.*\)/\1/p')"
+pkgversion="$(cat harmony.ipkg | sed -n 's/version = \(.*\)/\1/p')"
+idrversion="$(cat src/AppVersion.idr | sed -n 's/appVersion = "\(.*\)"/\1/p')"
 npmversion="$(cat package.json | jq -r .version)"
 
-if [ "$idrversion" == "$npmversion" ]; then
+if [ "$pkgversion" == "$npmversion" ] && [ "$pkgversion" == "$idrversion" ]; then
   exit 0
 else
-  echo "Idris manifest version (${idrversion}) and NPM package version (${npmversion}) do not agree!"
+  echo "Idris package manifest version (${pkgversion}), in-source version (${idrversion}) and NPM package version (${npmversion}) do not agree!"
   exit 1
 fi
 
