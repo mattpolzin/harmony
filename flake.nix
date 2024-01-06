6e9986cfc8dc65fc97ad530a9df1493689236add
Mathew Polzin <matt.polzin@gmail.com>
2024-01-06 13:36:56 -0600

    Add alejandra formatter and apply formatting.

--- flake.nix
+++ flake.nix
@@ -3,21 +3,34 @@
 
   inputs = {
     nixpkgs.url = github:NixOS/nixpkgs;
+
+    idris.url = github:mattpolzin/Idris2/fix-install-output-buildIdris;
+    idris.inputs.nixpkgs.follows = "nixpkgs";
+
+    alejandra.url = github:kamadorueda/alejandra;
+    alejandra.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs }:
-  let 
+  outputs = {
+    self,
+    nixpkgs,
+    alejandra,
+    idris,
+  }: let
     lib = nixpkgs.lib;
     forAllSystems = lib.genAttrs lib.systems.flakeExposed;
-  in
-  {
-    packages = forAllSystems (system: 
-      {
+  in {
+    packages = forAllSystems (
+      system: {
         harmony = with nixpkgs.legacyPackages.${system};
-          callPackage ./default.nix {};
+          callPackage ./default.nix {
+            idris2 = idris.packages.${system}.idris2;
+            buildIdris = idris.buildIdris.${system};
+          };
 
         default = self.packages.${system}.harmony;
       }
     );
+    formatter = forAllSystems (system: alejandra.packages.${system}.default);
   };
 }
