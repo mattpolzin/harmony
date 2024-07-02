29d306c5f2bfd9ca5bd0c3fbb0325586420abaaa
Mathew Polzin <matt.polzin@gmail.com>
2024-07-01 22:51:26 -0500

    bump version

--- node-packages.nix
+++ node-packages.nix
@@ -607,7 +607,7 @@
   args = {
     name = "_at_mattpolzin_slash_harmony";
     packageName = "@mattpolzin/harmony";
-    version = "4.3.0";
+    version = "4.4.0";
     src = ./.;
     dependencies = [
       sources."@kwsites/file-exists-1.1.1"
