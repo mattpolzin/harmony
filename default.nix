a363e1f6a89dca35e3543053e356fc37d3fd62be
Mathew Polzin <matt.polzin@gmail.com>
2024-09-08 22:46:55 -0500

    use new buildIdris' function

--- default.nix
+++ default.nix
@@ -2,7 +2,6 @@
   callPackage,
   fetchFromGitHub,
   git,
-  idris2Packages,
   buildIdris,
   lib,
   installShellFiles,
@@ -15,75 +14,61 @@ let
 
   idrisAdds = buildIdris {
     ipkgName = "idris-adds";
-    version = idrisAddsVersion;
     src = fetchFromGitHub {
       owner = "mattpolzin";
       repo = "idris-adds";
       rev = idrisAddsVersion;
       hash = "sha256-WEr6oRZ8+50G1qv7Kv62M4DRsgAa6x1BCODC1vDOQUY=";
     };
-    idrisLibraries = [ ];
-  };
-
-  harmonyPkg = buildIdris {
-    version = "5.0.0";
-    ipkgName = "harmony";
-    src = builtins.path {
-      path = ./.;
-      name = "harmony-pkg-src";
-    };
-
-    idrisLibraries =
-      let
-        ps = idris2Packages;
-      in
-      [
-        idrisAdds
-        ps.elab-util
-        ps.parser
-        ps.parser-json
-        ps.json
-      ];
-    nativeBuildInputs = [
-      installShellFiles
-      makeBinaryWrapper
-    ];
-    buildInputs = [
-      nodejs
-      git
-    ];
-
-    IDRIS2_DATA = "./support";
-
-    postInstall = ''
-      wrapProgram $out/bin/harmony \
-        --prefix PATH : ${
-          lib.makeBinPath [
-            nodeDependencies
-            git
-            "$out"
-          ]
-        } \
-        --prefix NODE_PATH : ${nodeDependencies}/lib/node_modules
-    '';
-
-    postFixup = ''
-      installShellCompletion --cmd harmony \
-        --bash <($out/bin/harmony --bash-completion-script) \
-        --zsh <($out/bin/harmony --zsh-completion-script) \
-    '';
-
-    installCheckPhase = ''
-      export harmony=$out/bin/harmony
-      INTERACTIVE="" make test
-    '';
-
-    meta = with lib; {
-      description = "Harmony GitHub collaboration tool";
-      homepage = "https://github.com/mattpolzin/harmony";
-      license = licenses.mit;
-      mainProgram = "harmony";
-    };
   };
 in
-harmonyPkg.executable
+buildIdris {
+  ipkgName = "harmony";
+  src = builtins.path {
+    path = ./.;
+    name = "harmony-pkg-src";
+  };
+
+  extraIdrisLibraries = [ idrisAdds ];
+
+  nativeBuildInputs = [
+    installShellFiles
+    makeBinaryWrapper
+  ];
+  buildInputs = [
+    nodejs
+    git
+  ];
+
+  IDRIS2_DATA = "./support";
+
+  postInstall = ''
+    wrapProgram $out/bin/harmony \
+      --prefix PATH : ${
+        lib.makeBinPath [
+          nodeDependencies
+          git
+          "$out"
+        ]
+      } \
+      --prefix NODE_PATH : ${nodeDependencies}/lib/node_modules
+  '';
+
+  postFixup = ''
+    installShellCompletion --cmd harmony \
+      --bash <($out/bin/harmony --bash-completion-script) \
+      --zsh <($out/bin/harmony --zsh-completion-script) \
+  '';
+
+  installCheckPhase = ''
+    export harmony=$out/bin/harmony
+    INTERACTIVE="" make test
+  '';
+
+  meta = with lib; {
+    description = "Harmony GitHub collaboration tool";
+    homepage = "https://github.com/mattpolzin/harmony";
+    license = licenses.mit;
+    mainProgram = "harmony";
+  };
+}
