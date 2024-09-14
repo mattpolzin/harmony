0021e37ea97a735378b792f4b6d70baa2e3c83e1
Mathew Polzin <matt.polzin@gmail.com>
2024-09-14 14:49:49 -0500

    no longer need to check nix version because it reads version from ipkg file

--- version-check.sh
+++ version-check.sh
@@ -3,12 +3,11 @@
 pkgversion="$(cat harmony.ipkg | sed -n 's/version = \(.*\)/\1/p')"
 idrversion="$(cat src/AppVersion.idr | sed -n 's/appVersion = "\(.*\)"/\1/p')"
 npmversion="$(cat package.json | jq -r .version)"
-nixversion="$(cat default.nix | sed -n 's/.*version = "\(.*\)";/\1/p')"
 
-if [ "$pkgversion" == "$npmversion" ] && [ "$pkgversion" == "$idrversion" ] && [ "$pkgversion" == "$nixversion" ]; then
+if [ "$pkgversion" == "$npmversion" ] && [ "$pkgversion" == "$idrversion" ]; then
   exit 0
 else
-  echo "Idris package manifest version (${pkgversion}), in-source version (${idrversion}), NPM package version (${npmversion}), and Nix version (${nixversion}) do not agree!"
+  echo "Idris package manifest version (${pkgversion}), in-source version (${idrversion}), and NPM package version (${npmversion}) do not agree!"
   exit 1
 fi
 
