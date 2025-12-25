f9f7760b566c0bdafd82b9b00189d912f3e72177
Mathew Polzin <matt.polzin@gmail.com>
2025-12-25 16:10:44 -0600

    stash before running manpage/readme checks

--- scripts/manpage-check.sh
+++ scripts/manpage-check.sh
@@ -2,6 +2,10 @@
 
 # check that the manpage was generated based on latest changes to its source
 # files.
+git stash || :
 make man/harmony.1
 git diff --exit-code
+EXITC=$?
+git stash pop || :
+exit $EXITC
 
--- scripts/readme-check.sh
+++ scripts/readme-check.sh
@@ -2,6 +2,10 @@
 
 # check that the README was generated based on latest changes to its source
 # files.
+git stash || :
 make README.md
 git diff --exit-code
+EXITC=$?
+git stash pop || :
+exit $EXITC
 
