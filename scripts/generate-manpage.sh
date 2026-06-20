62058ff965921055304439bbb6be870740b40284
Mathew Polzin <matt.polzin@gmail.com>
2026-06-20 14:42:41 -0500

    reformat file list pipeline in generate scripts for easier reading

--- scripts/generate-manpage.sh
+++ scripts/generate-manpage.sh
@@ -5,7 +5,14 @@ INDEX_SECTION_NAME="_${INDEX_SECTION_IDX}_INDEX"
 
 # Skip the installation section for the manpage. Presumably this thing has been
 # installed by the time the manpage is most useful.
-FILES="$(find ./docs -name '*.md' | sort | grep -v "${INDEX_SECTION_NAME}.md" | grep -v '_3.*Installation.md' | grep -v '_3_1.*Buildtime.md' | grep -v '_3_1.*Testing.md')"
+FILES="$(
+  find ./docs -name '*.md' | \
+    sort | \
+    grep -v "${INDEX_SECTION_NAME}.md" | \
+    grep -v '_3.*Installation.md' | \
+    grep -v '_3_1.*Buildtime.md' | \
+    grep -v '_3_1.*Testing.md'
+  )"
 
 OUTPUT_FILE="${1:-man/harmony.1}"
 
--- scripts/generate-readme.sh
+++ scripts/generate-readme.sh
@@ -4,7 +4,13 @@ INDEX_SECTION_IDX=1_9
 INDEX_SECTION_NAME="_${INDEX_SECTION_IDX}_INDEX"
 
 # Skip the manpage header and author sections and any existing index.
-FILES="$(find ./docs -name '*.md' | sort | grep -v '_0.*MANPAGE_HEADER.md' | grep -v '_5.*AUTHOR.md' | grep -v "${INDEX_SECTION_NAME}.md")"
+FILES="$(
+  find ./docs -name '*.md' | \
+    sort | \
+    grep -v '_0.*MANPAGE_HEADER.md' | \
+    grep -v '_5.*AUTHOR.md' | \
+    grep -v "${INDEX_SECTION_NAME}.md"
+  )"
 
 OUTPUT_FILE="${1:-README.md}"
 
